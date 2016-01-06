009f3d6e22c0bb5bf3bf18c3b5e653a8adacc4f3
andyskyba <a.skyba@drudesk.com>
2016-01-06 07:50:54 +0200

    Home - not done

---
+++ lesson02_task04.php
@@ -0,0 +1,24 @@
+<?php 
+
+//settings
+$quantity = 10;
+$minVal = 1;
+$maxVal = 100;
+$Arr = [];
+
+//fulfill array with random values
+//mt_rand() faster than rand()
+	for ($i = 0; $i < $quantity; $i++){
+		$Arr[$i] = mt_rand($minVal, $maxVal); 
+	}
+
+var_dump($Arr);
+
+//calculate sum $Arr
+$sumArr = 0;
+foreach ($Arr as $value) {
+	$sumArr += $value;
+}
+print "<br>$sumArr";
+
+
