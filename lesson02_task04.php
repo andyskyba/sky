62fdb5793755f7c1c9162fa15b6e3d27b10026fe
andyskyba <a.skyba@drudesk.com>
2016-01-19 22:06:58 +0200

    Finally stolen

--- lesson02_task04.php
+++ lesson02_task04.php
@@ -1,24 +1,44 @@
 <?php 
 
-//general settings
+//General setting
 $quantity = 10;
-$minVal = 1;
-$maxVal = 100;
-$Arr = [];
+$min = 0;
+$max = 100;
+$Arr = array(); 
 
-//fulfill array with random values
-//mt_rand() faster than rand()
-	for ($i = 0; $i < $quantity; $i++){
-		$Arr[$i] = mt_rand($minVal, $maxVal); 
-	}
-
-var_dump($Arr);
-
-//calculate sum $Arr
-$sumArr = 0;
-foreach ($Arr as $value) {
-	$sumArr += $value;
+for($i = 0; $i < $quantity; $i++){
+	$Arr[$i] = mt_rand(0,100);
 }
-print "<br>$sumArr";
+
+rsort($Arr,SORT_NUMERIC); 
+
+$halfSum = (int)(array_sum($Arr) / 2);
+//$halfSum = intval(array_sum($Arr) / 2); 
 
 
+$ArrOne = array(); 
+$ArrTwo = array(); 
+$sumOne = 0; 
+$sumTwo = 0; 
+
+foreach($Arr as $val){
+ 
+    if($sumOne <= $sumTwo || $sumTwo >= $halfSum){
+        $ArrOne[] = $val;
+        $sumOne += $val;
+    } else {
+        $ArrTwo[] = $val;
+        $sumTwo += $val;
+    }
+}
+
+//Display results
+var_dump($Arr);
+echo " :Sum original = ".array_sum($Arr);
+echo "<br><br>";
+var_dump($ArrOne);
+echo " :Sum = ".array_sum($ArrOne);
+echo "<br>";
+var_dump($ArrTwo);
+echo " :Sum = ".array_sum($ArrTwo);
+echo "<br>";
